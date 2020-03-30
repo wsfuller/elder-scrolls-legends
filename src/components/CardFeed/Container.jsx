f9e8c2b80a03a8612dace2e0ba2624ae7167885e
wfuller <wfuller87@gmail.com>
2020-03-29 22:50:30 -0700

    Updating terminology

--- src/components/CardFeed/Container.jsx
+++ src/components/CardFeed/Container.jsx
@@ -6,16 +6,19 @@ import { Error, Loading, NoResults, MaxResults } from '../GenericStates';
 import CardFeedList from './List';
 
 function CardFeedContainer() {
+  const MAX_RESULTS_TO_FETCH = 20;
   const [cards, setCards] = useState([]);
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [needCards, setNeedCards] = useState(true);
   const [reachedCardsLimit, setReachedCardsLimit] = useState(false);
-  const [url, setUrl] = useState('https://api.elderscrollslegends.io/v1/cards?page=1&pageSize=20');
+  const [url, setUrl] = useState(
+    `https://api.elderscrollslegends.io/v1/cards?page=1&pageSize=${MAX_RESULTS_TO_FETCH}`
+  );
 
   useEffect(() => {
     const getCards = async () => {
-      setLoading(true);
+      setIsLoading(true);
       try {
         const response = await axios.get(url);
         // eslint-disable-next-line no-underscore-dangle
@@ -33,7 +36,7 @@ function CardFeedContainer() {
         console.error(`There was an error fetching cards ${err}`);
         setError(true);
       } finally {
-        setLoading(false);
+        setIsLoading(false);
         setNeedCards(false);
       }
     };
@@ -41,34 +44,29 @@ function CardFeedContainer() {
     if (needCards) {
       getCards();
     }
-    // eslint-disable-next-line
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [needCards]);
-
-  const getMoreCards = () => {
-    setLoading(true);
-    setNeedCards(true);
-  };
 
   window.onscroll = debounce(() => {
     const WINDOW_HEIGHT = window.innerHeight + document.documentElement.scrollTop;
     const DOCUMENT_HEIGHT = document.documentElement.offsetHeight;
 
     if (WINDOW_HEIGHT >= DOCUMENT_HEIGHT && !reachedCardsLimit) {
-      getMoreCards();
+      setNeedCards(true);
     }
   }, 300);
 
   if (error) {
     return <Error message="We're sorry but something went wrong trying to fetch your cards" />;
   }
-  if (!loading && cards.length === 0) {
+  if (!isLoading && cards.length === 0) {
     return <NoResults />;
   }
   return (
     <Fragment>
       <CardFeedList cards={cards} />
       {reachedCardsLimit && <MaxResults message="There are no more cards to show" />}
-      {loading && <Loading />}
+      {isLoading && <Loading />}
     </Fragment>
   );
 }
