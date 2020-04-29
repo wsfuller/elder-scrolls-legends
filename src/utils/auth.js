f58e1e944821dab38a09a8f31eab3db7b7ff1ede
wfuller <wfuller87@gmail.com>
2020-04-28 17:56:38 -0700

    Remove JS Cookie dep

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,14 @@
-import Cookies from 'js-cookie';
+const tokenName = 'esl_user_token';
 
-// let history = useHistory();
+export const handleAuthentication = () => {
+  console.log('handle authentication');
+};
 
 export const handleLogin = (token) => {
   return new Promise((resolve, reject) => {
-    const tokenName = 'esl_token';
+    localStorage.setItem(tokenName, token);
 
-    Cookies.set(tokenName, token);
-
-    if (Cookies.get(tokenName)) {
+    if (localStorage.getItem(tokenName)) {
       resolve(true);
     } else {
       reject(Error('error setting cookie'));
@@ -16,26 +16,9 @@ export const handleLogin = (token) => {
   });
 };
 
-// export const handleLogin = (token) => {
-//   // SAVE TOKEN TO COOKIE
-
-//   // REDIRECT USER TO PROFILE
-//   history.push('/');
-// };
-
 export const handleLogout = () => {
-  // REMOVE COOKIE TOKEN
-  // REDIRECT USER TO LOGIN PAGE
-  // window.localStorage.setItem('logout', Date.now());
-};
-
-export const redirectUser = (ctx, location) => {
-  // if (ctx.req) {
-  //   ctx.res.writeHead(302, { Location: location });
-  //   ctx.res.end();
-  // } else {
-  //   Router.push(location);
-  // }
+  localStorage.setItem('logout', Date.now());
+  localStorage.removeItem(tokenName);
 };
 
 export const validateEmail = (email) => {
