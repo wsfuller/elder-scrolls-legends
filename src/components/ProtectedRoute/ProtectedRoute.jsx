9e33db497530f20dfd34c3b37f30d0a8deb2499b
wfuller <wfuller87@gmail.com>
2020-04-28 17:56:58 -0700

    Protected Route component

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Redirect, Route } from 'react-router-dom';
+
+function ProtectedRoute({ children, isUserAuthenticated }) {
+  return (
+    <Route
+      render={({ location }) =>
+        isUserAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+ProtectedRoute.propTypes = {
+  isUserAuthenticated: PropTypes.bool.isRequired,
+};
+
+export default ProtectedRoute;
